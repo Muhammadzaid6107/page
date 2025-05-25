bc4b10f8a525edef772f1640fe56d4a3d7c1a2d2
Muhammadzaid6107 <zaid.6107zaid@gmail.com>
2025-05-25 23:07:41 +0500

    firebase implement

---
+++ pages/src/firebase/auth.js
@@ -0,0 +1,24 @@
+import { auth } from "./firebase";
+import { createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, updatePassword } from "firebase/auth";
+
+export const doCreateUserWithEmailAndPassword = async (email, password) => {
+
+    return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInUserWithEmailAndPassword =(email , password) =>{
+    return signInWithEmailAndPassword(auth, email, password);
+};
+
+// export const doPasswordReset = (email) => {
+//     return sendPasswordResetEmail(auth, email);
+// };
+// export const doPasswordChange = (password) =>{
+//     return updatePassword(auth.currentUser, password)
+// };
+
+// export const doSendEmailVerification =() =>{
+//     return sendEmailVerification(auth.currentUser,{
+//         url: ${window.loction.origin}/home,
+//     });
+// };
